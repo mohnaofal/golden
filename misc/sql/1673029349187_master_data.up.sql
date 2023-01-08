3234b8c284a99ed4245cb201845e79da0621545c
Moh. Naofal Nurul Huda <moh.naofal@gmail.com>
2023-01-08 23:15:44 +0700

    insert master data

--- misc/sql/1673029349187_master_data.up.sql
+++ misc/sql/1673029349187_master_data.up.sql
@@ -1,5 +1,38 @@
-INSERT INTO rekening (rek_norek, rek_updated_at)
+-- insert rekening
+INSERT INTO rekening (rek_norek, rek_saldo, rek_updated_at)
     SELECT
-    	'r001', 
+    	'r001',
+		1,
     	(SELECT EXTRACT(EPOCH FROM  now()))
+;
+
+-- insert harga
+INSERT INTO harga (harga_admin_id, harga_topup, harga_buyback, harga_date)
+	SELECT
+		'a001',
+		100000,
+		90000,
+		(SELECT EXTRACT(EPOCH FROM  now()))
+;
+
+INSERT INTO transaksi (trx_date, trx_type, trx_gram, trx_harga_topup, trx_harga_buyback, trx_saldo, trx_norek)
+	SELECT
+		(SELECT EXTRACT(EPOCH FROM  now())),
+		'topup',
+		1,
+		100000,
+		90000,
+		2, 
+		'r001'
+;
+
+INSERT INTO transaksi (trx_date, trx_type, trx_gram, trx_harga_topup, trx_harga_buyback, trx_saldo, trx_norek)
+	SELECT
+		(SELECT EXTRACT(EPOCH FROM  now())),
+		'buyback',
+		1,
+		100000,
+		90000,
+		1, 
+		'r001'
 ;
